8b56251d401f3cf659efdc146f906958b84d398b
Minh Chau Hoang <kidphys@gmail.com>
2016-08-15 00:13:51 +0700

    Link up view1 with API, still need refactor

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,51 +2,51 @@
 
 var SMARTP_API_URL = 'https://script.google.com/macros/s/AKfycbweSsf8cPPk__HDZiWxszJ2HdV6s3zJU8WKEErqm41TT3CeI9k/exec';
 
-/**
-Yet another version of API which use angular $http provder
-This has to change. Do we want to depend on Angular 1 in the future??
-*/
-function smartP_api($http) {
-    function _build_engineP_params(input) {
-        var symbols = [];
-        var quantities = [];
+// /**
+// Yet another version of API which use angular $http provder
+// This has to change. Do we want to depend on Angular 1 in the future??
+// */
+// function smartP_api($http) {
+//     function _build_engineP_params(input) {
+//         var symbols = [];
+//         var quantities = [];
 
-        for(var key in input) {
-            symbols.push(key);
-            quantities.push(input[key]);
-        }
+//         for(var key in input) {
+//             symbols.push(key);
+//             quantities.push(input[key]);
+//         }
 
-        return 'symbols=' + symbols.join(',') + '&' + 'quantities=' + quantities.join(',');
-    }
+//         return 'symbols=' + symbols.join(',') + '&' + 'quantities=' + quantities.join(',');
+//     }
 
-    return {
-        /**
-        Accept a json obj:
-        {symbols: {VND: 100, SSI: 100}, cash: 1000}
+//     return {
+//         /**
+//         Accept a json obj:
+//         {symbols: {VND: 100, SSI: 100}, cash: 1000}
 
-        Callback with:
-        {risk: 0.9, expectedReturn: -1.2}
-        */
-        compute: function(input, callback, errorCallback) {
-            var url = SMARTP_API_URL + '?' + _build_engineP_params(input);
-            console.log('Start computing...', url);
-            $http({
-              method: 'GET',
-              url: url
-            }).then(function successCallback(response) {
-                callback({
-                    risk: response.data.risk,
-                    expectedReturn: response.data.expectedReturn
-                });
-              }, function errorCallback(response) {
-                console.log('Error ', response);
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-              });
+//         Callback with:
+//         {risk: 0.9, expectedReturn: -1.2}
+//         */
+//         compute: function(input, callback, errorCallback) {
+//             var url = SMARTP_API_URL + '?' + _build_engineP_params(input);
+//             console.log('Start computing!...', url);
+//             $http({
+//               method: 'GET',
+//               url: url
+//             }).then(function successCallback(response) {
+//                 callback({
+//                     risk: response.data.risk,
+//                     expectedReturn: response.data.expectedReturn
+//                 });
+//               }, function errorCallback(response) {
+//                 console.log('Error ', response);
+//                 // called asynchronously if an error occurs
+//                 // or server returns response with an error status.
+//               });
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
 
 angular.module('myApp.view2', ['ngRoute'])
