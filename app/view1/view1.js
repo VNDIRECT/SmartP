b9cbdc9a9a6c37a408b85d7ecf901d295e938a9d
Minh Chau Hoang <kidphys@gmail.com>
2016-08-15 00:49:34 +0700

    Wired both view1 and view2 with engineP

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,57 +1,5 @@
 'use strict';
 
-/**
-Yet another version of API which use angular $http provder
-This has to change. Do we want to depend on Angular 1 in the future??
-*/
-function smartP_api($http) {
-    function _build_engineP_params(input) {
-        var symbols = [];
-        var quantities = [];
-
-        for(var key in input) {
-            symbols.push(key);
-            quantities.push(input[key]);
-        }
-
-        return 'symbols=' + symbols.join(',') + '&' + 'quantities=' + quantities.join(',');
-    }
-
-    function _format_number(input) {
-        console.log('Formatting', input);
-        return input.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-    }
-
-    return {
-        /**
-        Accept a json obj:
-        {symbols: {VND: 100, SSI: 100}, cash: 1000}
-
-        Callback with:
-        {risk: 0.9, expectedReturn: -1.2}
-        */
-        compute: function(input, callback, errorCallback) {
-            var url = SMARTP_API_URL + '?' + _build_engineP_params(input);
-            console.log('Start computing view 1...', url);
-            $http({
-              method: 'GET',
-              url: url
-            }).then(function successCallback(response) {
-                callback({
-                    risk: _format_number(response.data.risk),
-                    expectedReturn: _format_number(response.data.expectedReturn)
-                });
-              }, function errorCallback(response) {
-                console.log('Error ', response);
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-              });
-
-        }
-    }
-}
-
-
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -61,11 +9,10 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('View1Ctrl', ['$scope', 'engineP', function($scope, engineP) {
     $scope.symbol = $scope.symbol || 'VND';
     $scope.compute_smartP = function() {
-        var api = smartP_api($http);
-        api.compute({VND: $scope.vnd_quantity || 100, SSI: $scope.ssi_quantity || 200}, function(result) {
+        engineP.compute({VND: $scope.vnd_quantity || 100, SSI: $scope.ssi_quantity || 200}, function(result) {
             console.log('Computing process has been done with result: ', result);
             $scope.risk = result.risk;
             $scope.expectedReturn = result.expectedReturn;
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,53 +2,6 @@
 
 var SMARTP_API_URL = 'https://script.google.com/macros/s/AKfycbweSsf8cPPk__HDZiWxszJ2HdV6s3zJU8WKEErqm41TT3CeI9k/exec';
 
-// /**
-// Yet another version of API which use angular $http provder
-// This has to change. Do we want to depend on Angular 1 in the future??
-// */
-// function smartP_api($http) {
-//     function _build_engineP_params(input) {
-//         var symbols = [];
-//         var quantities = [];
-
-//         for(var key in input) {
-//             symbols.push(key);
-//             quantities.push(input[key]);
-//         }
-
-//         return 'symbols=' + symbols.join(',') + '&' + 'quantities=' + quantities.join(',');
-//     }
-
-//     return {
-//         /**
-//         Accept a json obj:
-//         {symbols: {VND: 100, SSI: 100}, cash: 1000}
-
-//         Callback with:
-//         {risk: 0.9, expectedReturn: -1.2}
-//         */
-//         compute: function(input, callback, errorCallback) {
-//             var url = SMARTP_API_URL + '?' + _build_engineP_params(input);
-//             console.log('Start computing!...', url);
-//             $http({
-//               method: 'GET',
-//               url: url
-//             }).then(function successCallback(response) {
-//                 callback({
-//                     risk: response.data.risk,
-//                     expectedReturn: response.data.expectedReturn
-//                 });
-//               }, function errorCallback(response) {
-//                 console.log('Error ', response);
-//                 // called asynchronously if an error occurs
-//                 // or server returns response with an error status.
-//               });
-
-//         }
-//     }
-// }
-
-
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -58,11 +11,10 @@ angular.module('myApp.view2', ['ngRoute'])
     });
 }])
 
-.controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('View2Ctrl', ['$scope', 'engineP', function($scope, engineP) {
     $scope.symbol = $scope.symbol || 'VND';
     $scope.compute_smartP = function() {
-        var api = smartP_api($http);
-        api.compute({VND: $scope.vnd_quantity || 0, SSI: $scope.ssi_quantity || 0}, function(result) {
+        engineP.compute({VND: $scope.vnd_quantity || 0, SSI: $scope.ssi_quantity || 0}, function(result) {
             console.log('Computing process has been done with result: ', result);
             $scope.risk = result.risk;
             $scope.expectedReturn = result.expectedReturn;
