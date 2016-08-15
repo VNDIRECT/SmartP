80c97c84c6ddd14d2f1b8c1ceadfa83e6b0c04ce
Minh Chau Hoang <kidphys@gmail.com>
2016-08-15 15:23:46 +0700

    Add symbol feature

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,6 +11,59 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', 'engineP', 'tradeapi', function($scope, engineP, tradeapi) {
     $scope.symbol = $scope.symbol || 'VND';
+
+    /**
+    Handle portfolio logic:
+    If symbol exists when added, increase the current quantity instead
+    */
+    function create_portfolio() {
+        var init_data =
+            [
+                {symbol: 'VND', quantity: 200},
+                {symbol: 'SSI', quantity: 100},
+            ];
+        return {
+            data: [],
+
+            add: function(symbol, quantity) {
+                for(var i = 0; i < this.data.length; i++) {
+                    if(this.data[i].symbol == symbol) {
+                        this.data[i].quantity += quantity;
+                        return;
+                    }
+                }
+                this.data.push({symbol: symbol, quantity: quantity});
+            },
+
+            /**
+            Load the init data into data
+            */
+            init: function() {
+                this.data.length = 0;
+                for(var i = 0; i < init_data.length; i++) {
+                    this.add(init_data[i].symbol, init_data[i].quantity);
+                }
+                return this;
+            }
+
+        }
+    };
+
+    $scope.portfolio = create_portfolio().init();
+
+    $scope.add_symbol = function() {
+        // simple validation to avoid empty string symbol input
+        if($scope.pending_symbol.length > 0) {
+            $scope.portfolio.add($scope.pending_symbol, $scope.pending_quantity);
+            $scope.pending_quantity = 0;
+            $scope.pending_symbol =  '';
+        }
+    }
+
+    $scope.reset = function() {
+        $scope.portfolio.init();
+    }
+
     $scope.compute_smartP = function() {
         tradeapi.login('thangnt.nhtck47', 'vnds@1234')
         .then(
@@ -25,7 +78,7 @@ angular.module('myApp.view1', ['ngRoute'])
             }
         );
 
-        console.log('Logging in...');
+
         engineP.compute({VND: $scope.vnd_quantity || 100, SSI: $scope.ssi_quantity || 200}, function(result) {
             console.log('Computing process has been done with result: ', result);
             $scope.risk = result.risk;
@@ -33,5 +86,6 @@ angular.module('myApp.view1', ['ngRoute'])
         }, function errorCallback(error) {
             console.log('error while compute smartP', error);
         });
+
     }
 }]);
