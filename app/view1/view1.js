14cb574df3ec3488c8396b4fb8f5a7463e0b542f
linhbuimai <linhmai236@outlook.com>
2016-08-15 09:47:34 +0700

    Fix js merge error

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,15 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', 'engineP', function($scope, engineP) {
+    $scope.symbol = $scope.symbol || 'VND';
+    $scope.compute_smartP = function() {
+        engineP.compute({VND: $scope.vnd_quantity || 100, SSI: $scope.ssi_quantity || 200}, function(result) {
+            console.log('Computing process has been done with result: ', result);
+            $scope.risk = result.risk;
+            $scope.expectedReturn = result.expectedReturn;
+        }, function errorCallback(error) {
+            console.log('error while compute smartP', error);
+        });
+    }
 }]);
